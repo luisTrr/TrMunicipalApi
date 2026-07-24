76f7ef5cdeb62daeae1199692ed980666bd06f56
luisTrr <drax026@gmail.com>
2026-07-24 12:11:48 -0400

    feat(auth): Adding endpoints for authentication

---
+++ Base.Api/EndPoints/Authentication/AuthEndpoints.cs
@@ -0,0 +1,57 @@
+﻿using Base.Api.EndPoints.Common;
+using Base.Aplication.Services.Authentication;
+using Base.Domain.Dtos.Authentication;
+
+namespace Base.Api.EndPoints.Authentication;
+
+public static class AuthEndpoints
+{
+    internal static void MapAuthEndpoints(
+        this WebApplication webApp)
+    {
+        webApp.MapGroup("auth")
+            .WithTags("AUTHENTICATION")
+            .MapAuthGroupEndpoints();
+    }
+
+
+    private static void MapAuthGroupEndpoints(
+        this RouteGroupBuilder builder)
+    {
+        builder.MapPost(
+            "/register",
+            (
+                    RegisterRequestDto request,
+                    AuthService service) =>
+                service.RegisterAsync(request)
+                    .ToApiResult());
+
+
+        builder.MapPost(
+            "/login",
+            (
+                    LoginRequestDto request,
+                    AuthService service) =>
+                service.LoginAsync(request)
+                    .ToApiResult());
+
+
+        builder.MapPost(
+            "/refresh-token",
+            (
+                    RefreshTokenRequestDto request,
+                    AuthService service) =>
+                service.RefreshTokenAsync(request)
+                    .ToApiResult());
+
+
+        builder.MapPost(
+            "/logout",
+            (
+                    RefreshTokenRequestDto request,
+                    AuthService service) =>
+                service.LogoutAsync(
+                        request.RefreshToken)
+                    .ToApiResult());
+    }
+}
